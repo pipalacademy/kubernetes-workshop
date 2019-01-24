2248b63c54d5bf1deffda03a5c2242fc72f33797
Nabarun Pal <pal.nabarun95@gmail.com>
2019-01-24 10:38:01 +0530

    Adds application for showing health and liveliness checks

---
+++ examples/counter-health/main.go
@@ -0,0 +1,73 @@
+// Simple counter over http
+//
+// This dies when the count reaches 13.
+// Useful to demonstrate logs and auto-healing of k8s.
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "net/http"
+    "time"
+)
+
+type CountInfo struct {
+    Hostname    string
+    Pid         int
+    Count       int
+}
+
+var Count = 0
+var Healthy = true
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    Count += 1
+    if (Count == 10) {
+        Healthy = false
+    }
+
+    hostname, err := os.Hostname()
+    if err != nil {
+        panic(err)
+    }
+
+    pid := os.Getpid()
+
+    info := CountInfo{
+        hostname,
+        pid,
+        Count,
+    }
+
+    infoJson, err := json.Marshal(info)
+    if err != nil {
+        log.Fatal("Cannot encode to JSON ", err)
+    }
+
+
+    log.Printf("Host=%s Pid=%d Count=%d\n", hostname, pid, Count)
+    fmt.Fprintf(w, "%s\n", infoJson)
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+    if (Healthy == false) {
+        w.WriteHeader(http.StatusInternalServerError)
+        w.Write([]byte("unhealthy\n"))
+        return
+    }
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("healthy\n"))
+}
+
+func init() {
+    time.Sleep(5*time.Second)
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.HandleFunc("/health", health)
+    log.Println("Starting the server...")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
