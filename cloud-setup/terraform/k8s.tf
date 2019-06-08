77b9ad9e3e95812ed61397cf62d38ee9b205a5df
Anand Chitipothu <anandology@gmail.com>
2019-06-08 23:58:55 +0530

    Commented out the terraform resources that use dependent providers.
    Terraform is not very good at handling dependent providers. The plan is
    to convert them into multi-stage terraform steps.

--- cloud-setup/terraform/k8s.tf
+++ cloud-setup/terraform/k8s.tf
@@ -13,73 +13,75 @@ resource "digitalocean_kubernetes_cluster" "k8s-workshop" {
   }
 }
 
-provider "kubernetes" {
-  host = "${digitalocean_kubernetes_cluster.k8s-workshop.endpoint}"
+# provider "kubernetes" {
+#   host = "${digitalocean_kubernetes_cluster.k8s-workshop.endpoint}"
 
-  client_certificate     = "${base64decode(digitalocean_kubernetes_cluster.k8s-workshop.kube_config.0.client_certificate)}"
-  client_key             = "${base64decode(digitalocean_kubernetes_cluster.k8s-workshop.kube_config.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(digitalocean_kubernetes_cluster.k8s-workshop.kube_config.0.cluster_ca_certificate)}"
-}
+#   # client_certificate     = "${base64decode(digitalocean_kubernetes_cluster.k8s-workshop.kube_config.0.client_certificate)}"
+#   # client_key             = "${base64decode(digitalocean_kubernetes_cluster.k8s-workshop.kube_config.0.client_key)}"
+#   # cluster_ca_certificate = "${base64decode(digitalocean_kubernetes_cluster.k8s-workshop.kube_config.0.cluster_ca_certificate)}"
+#   client_certificate     = "${base64decode(file("kube/client-cert.pem"))}"
+#   client_key             = "${base64decode(file("kube/client-key.pem"))}"
+#   cluster_ca_certificate = "${base64decode(file("kube/cluster-ca-cert.pem"))}"}
 
-resource "kubernetes_namespace" "ns" {
-  metadata {
-    name   = "${var.names[count.index]}"
-  }
-  count   = "${var.k8s_enabled * var.num_nodes}"
+# resource "kubernetes_namespace" "ns" {
+#   metadata {
+#     name   = "${var.names[count.index]}"
+#   }
+#   count   = "${var.k8s_enabled * var.num_nodes}"
 
-  depends_on = ["digitalocean_kubernetes_cluster.k8s-workshop"]
-}
+#   depends_on = ["digitalocean_kubernetes_cluster.k8s-workshop"]
+# }
 
-resource "kubernetes_service_account" "sa" {
-  metadata {
-    name = "${var.names[count.index]}"
-    namespace = "${var.names[count.index]}"
-  }
+# resource "kubernetes_service_account" "sa" {
+#   metadata {
+#     name = "${var.names[count.index]}"
+#     namespace = "${var.names[count.index]}"
+#   }
 
-  count   = "${var.k8s_enabled * var.num_nodes}"
-  depends_on = ["kubernetes_namespace.ns"]  
-}
+#   count   = "${var.k8s_enabled * var.num_nodes}"
+#   depends_on = ["kubernetes_namespace.ns"]
+# }
 
 
-data "kubernetes_secret" "sa-secret" {
-  metadata = {
-    name      = "${element(kubernetes_service_account.sa.*.default_secret_name, count.index)}"
-    namespace = "${var.names[count.index]}"
-  }
-  count   = "${var.k8s_enabled * var.num_nodes}"  
-}
+# data "kubernetes_secret" "sa-secret" {
+#   metadata = {
+#     name      = "${element(kubernetes_service_account.sa.*.default_secret_name, count.index)}"
+#     namespace = "${var.names[count.index]}"
+#   }
+#   count   = "${var.k8s_enabled * var.num_nodes}"
+# }
 
-resource "kubernetes_cluster_role_binding" "k8s" {
-    metadata {
-      name = "${var.names[count.index]}"
-    }
-    role_ref {
-        api_group = "rbac.authorization.k8s.io"
-        kind = "ClusterRole"
-        name = "cluster-admin"
-    }
-    subject {
-        kind = "ServiceAccount"
-        name = "${var.names[count.index]}"
-        namespace = "${var.names[count.index]}"
-    }
-    count   = "${var.k8s_enabled * var.num_nodes}"  
-}
+# resource "kubernetes_cluster_role_binding" "k8s" {
+#     metadata {
+#       name = "${var.names[count.index]}"
+#     }
+#     role_ref {
+#         api_group = "rbac.authorization.k8s.io"
+#         kind = "ClusterRole"
+#         name = "cluster-admin"
+#     }
+#     subject {
+#         kind = "ServiceAccount"
+#         name = "${var.names[count.index]}"
+#         namespace = "${var.names[count.index]}"
+#     }
+#     count   = "${var.k8s_enabled * var.num_nodes}"
+# }
 
-resource "kubernetes_secret" "klickr" {
-  metadata {
-    name = "klickr"
-    namespace = "${var.names[count.index]}"
-  }
+# resource "kubernetes_secret" "klickr" {
+#   metadata {
+#     name = "klickr"
+#     namespace = "${var.names[count.index]}"
+#   }
 
-  data {
-    "AWS_REGION_NAME" = "sgp1",
-    "AWS_ENDPOINT_URL" = "https://sgp1.digitaloceanspaces.com"
-    "AWS_ACCESS_KEY_ID" = "${var.digitalocean_spaces_key}"
-    "AWS_SECRET_ACCESS_KEY" = "${var.digitalocean_spaces_secret}"
-    "STORAGE_S3_BUCKET" = "${element(digitalocean_spaces_bucket.klickr.*.name, count.index)}"
-    "STORAGE_S3_BASE_URL" = "https://${element(digitalocean_spaces_bucket.klickr.*.bucket_domain_name, count.index)}"
-  }
+#   data {
+#     "AWS_REGION_NAME" = "sgp1",
+#     "AWS_ENDPOINT_URL" = "https://sgp1.digitaloceanspaces.com"
+#     "AWS_ACCESS_KEY_ID" = "${var.digitalocean_spaces_key}"
+#     "AWS_SECRET_ACCESS_KEY" = "${var.digitalocean_spaces_secret}"
+#     "STORAGE_S3_BUCKET" = "${element(digitalocean_spaces_bucket.klickr.*.name, count.index)}"
+#     "STORAGE_S3_BASE_URL" = "https://${element(digitalocean_spaces_bucket.klickr.*.bucket_domain_name, count.index)}"
+#   }
 
-  count   = "${var.k8s_enabled * var.num_nodes}"
-}
+#   count   = "${var.k8s_enabled * var.num_nodes}"
+# }
--- cloud-setup/terraform/main.tf
+++ cloud-setup/terraform/main.tf
@@ -26,12 +26,12 @@ resource "digitalocean_record" "subdomain" {
   count  = "${var.num_nodes}"  
 }
 
-resource "digitalocean_record" "lb" {
-  domain = "pipal.in"
-  type   = "A"
-  name   = "k8s"
-  value  = "${var.lb_ip}"
-}
+# resource "digitalocean_record" "lb" {
+#   domain = "pipal.in"
+#   type   = "A"
+#   name   = "k8s"
+#   value  = "${var.lb_ip}"
+# }
 
 resource "digitalocean_record" "wildcard" {
   domain = "pipal.in"
@@ -58,54 +58,54 @@ resource "digitalocean_spaces_bucket" "klickr" {
 }
 
 # Create a new database cluster
-resource "digitalocean_database_cluster" "klickr" {
-  name       = "klickr"
-  engine     = "pg"
-  version    = "11"
-  size       = "db-s-1vcpu-1gb"
-  region     = "blr1"
-  node_count = 1
-}
+# resource "digitalocean_database_cluster" "klickr" {
+#   name       = "klickr"
+#   engine     = "pg"
+#   version    = "11"
+#   size       = "db-s-1vcpu-1gb"
+#   region     = "blr1"
+#   node_count = 1
+# }
 
-resource "digitalocean_record" "klickr-db" {
-  domain = "pipal.in"
-  name   = "klickr-db"
+# resource "digitalocean_record" "klickr-db" {
+#   domain = "pipal.in"
+#   name   = "klickr-db"
 
-## DO is refusing to create CNAME to their own domans
-#  type   = "CNAME"
-#  value  = "${digitalocean_database_cluster.klickr.host}."
-  type = "A"
-  value = "68.183.90.205"
-}
+# ## DO is refusing to create CNAME to their own domans
+# #  type   = "CNAME"
+# #  value  = "${digitalocean_database_cluster.klickr.host}."
+#   type = "A"
+#   value = "68.183.90.205"
+# }
 
 
-provider "postgresql" {
-  host            = "${digitalocean_database_cluster.klickr.host}"
-  port            = "${digitalocean_database_cluster.klickr.port}"
-  database        = "${digitalocean_database_cluster.klickr.database}"
-  username        = "${digitalocean_database_cluster.klickr.user}"
-  password        = "${digitalocean_database_cluster.klickr.password}"
-  sslmode         = "require"
-  superuser = false
-  connect_timeout = 15
-}
+# provider "postgresql" {
+#   host            = "${digitalocean_database_cluster.klickr.host}"
+#   port            = "${digitalocean_database_cluster.klickr.port}"
+#   database        = "${digitalocean_database_cluster.klickr.database}"
+#   username        = "${digitalocean_database_cluster.klickr.user}"
+#   password        = "${digitalocean_database_cluster.klickr.password}"
+#   sslmode         = "require"
+#   superuser = false
+#   connect_timeout = 15
+# }
 
-resource "postgresql_role" "pipal" {
-  name     = "pipal"
-  login    = true
-  password = "${var.db_password}"
-#  superuser = false
-  depends_on = ["digitalocean_database_cluster.klickr"]
-}
+# resource "postgresql_role" "pipal" {
+#   name     = "pipal"
+#   login    = true
+#   password = "${var.db_password}"
+# #  superuser = false
+#   depends_on = ["digitalocean_database_cluster.klickr"]
+# }
 
-resource "postgresql_database" "klickr-db" {
-  name              = "${var.names[count.index]}"
-  owner             = "${postgresql_role.pipal.name}"
-  template          = "template0"
-  lc_collate        = "C"
-  connection_limit  = -1
-  allow_connections = true
+# resource "postgresql_database" "klickr-db" {
+#   name              = "${var.names[count.index]}"
+#   owner             = "${postgresql_role.pipal.name}"
+#   template          = "template0"
+#   lc_collate        = "C"
+#   connection_limit  = -1
+#   allow_connections = true
 
-  count  = "${var.num_nodes}"
+#   count  = "${var.num_nodes}"
 
-}
+# }
